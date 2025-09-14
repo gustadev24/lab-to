e0f729354276d2301b367acede7f7385ca0b1366
gustadev24 <lgsc21211@gmail.com>
2025-09-13 22:57:21 -0500

    test: prove invalid tokens

--- lab01/tests/src/calculator-core.spec.cpp
+++ lab01/tests/src/calculator-core.spec.cpp
@@ -6,3 +6,9 @@ TEST(CalculatorCoreTest, ComputeBasic) {
   solver.solve({"3", "4", "+", "2", "+"});
   EXPECT_DOUBLE_EQ(solver.getResult(), 9.0);
 }
+
+TEST(CalculatorCoreTest, ComputeInvalid) {
+  CalculatorCore solver;
+  EXPECT_THROW(solver.solve({"12", "+", "*", "34"}), std::runtime_error);
+  EXPECT_THROW(solver.solve({"abc", "+", "12"}), std::runtime_error);
+}
