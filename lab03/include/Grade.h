b32faaceccb63686d90bd242bf5191ea568d85e9
gustadev24 <lgsc21211@gmail.com>
2025-09-27 12:58:03 -0500

    ops: create Grade class, enum is not enough

--- lab03/include/Grade.h
+++ lab03/include/Grade.h
@@ -1,9 +1,21 @@
 #pragma once
-enum Grade {
-  FIRST,
-  SECOND,
-  THIRD,
-  FOURTH,
-  FIFTH,
-  SIXTH,
+#include <string>
+class Grade {
+  private:
+    std::string id;
+    std::string name;
+
+    Grade(std::string id, std::string name);
+
+  public:
+    static const Grade FIRST;
+    static const Grade SECOND;
+    static const Grade THIRD;
+    static const Grade FOURTH;
+    static const Grade FIFTH;
+    static const Grade SIXTH;
+
+    std::string getId();
+    std::string getName();
+    std::string toString();
 };
---
+++ lab03/src/Grade.cpp
@@ -0,0 +1,25 @@
+#include "Grade.h"
+
+Grade::Grade(std::string id, std::string name) {
+    this->id = id;
+    this->name = name;
+}
+
+const Grade Grade::FIRST = Grade("1", "First");
+const Grade Grade::SECOND = Grade("2", "Second");
+const Grade Grade::THIRD = Grade("3", "Third");
+const Grade Grade::FOURTH = Grade("4", "Fourth");
+const Grade Grade::FIFTH = Grade("5", "Fifth");
+const Grade Grade::SIXTH = Grade("6", "Sixth");
+
+std::string Grade::getId() {
+    return this->id;
+}
+
+std::string Grade::getName() {
+    return this->name;
+}
+
+std::string Grade::toString() {
+    return "Grade{id='" + this->id + "', name='" + this->name + "'}";
+}
