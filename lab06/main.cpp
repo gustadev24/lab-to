c0fa884f691ac523bf3600a66a47e31daa2ffb22
gustadev24 <lgsc21211@gmail.com>
2025-10-18 13:08:34 -0500

    chore: connect addTaskButton with quit application action

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -1,26 +1,12 @@
 #include <QApplication>
-#include "comunicacion.h"
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    // MainWindow w;
-    // w.show();
+    MainWindow w;
+    w.show();
 
-    QObject *person = new QObject;
-    Comunicacion *pepe = new Comunicacion(person);
-    Comunicacion *jose = new Comunicacion(person);
-
-    pepe->setNombre("Pepe");
-    jose->setNombre("Jose");
-
-    QObject::connect(pepe, SIGNAL(enviarmensaje(QString)), jose, SLOT(escuchar(QString)));
-    QObject::connect(jose, SIGNAL(enviarmensaje(QString)), pepe, SLOT(escuchar(QString)));
-
-    pepe->conversar("Buenos días");
-    jose->conversar("¿Cómo vas?");
-
-    delete person;
 
     return a.exec();
 }
