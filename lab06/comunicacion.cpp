27ef3599d4787aaa6cc548e00ee001b4c6a80116
gustadev24 <lgsc21211@gmail.com>
2025-10-18 12:27:27 -0500

    chore: implement slots and signals for comunication

--- lab06/comunicacion.cpp
+++ lab06/comunicacion.cpp
@@ -1,5 +1,14 @@
 #include "comunicacion.h"
+#include <QDebug>
 
 Comunicacion::Comunicacion(QObject *parent)
     : QObject{parent}
 {}
+
+void Comunicacion::escuchar(const QString &str) {
+    qDebug() << "Estoy escuchando..." << str;
+}
+
+void Comunicacion::conversar(const QString &conversa) {
+    emit enviarmensaje(conversa);
+}
