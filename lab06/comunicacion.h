8ffac691713cd3d7e9d78d5e6401647e1c790e96
gustadev24 <lgsc21211@gmail.com>
2025-10-18 12:26:45 -0500

    chore: add slots and signals declarations for link and comunicate

--- lab06/comunicacion.h
+++ lab06/comunicacion.h
@@ -6,10 +6,21 @@
 class Comunicacion : public QObject
 {
     Q_OBJECT
+private:
+    QString strnombre;
 public:
     explicit Comunicacion(QObject *parent = nullptr);
 
+    void setNombre(const QString &nomb) {
+        strnombre = nomb;
+    }
+
+    void conversar(const QString &conversa);
+
 signals:
+    void enviarmensaje(QString);
+public slots:
+    void escuchar(const QString &str);
 };
 
 #endif // COMUNICACION_H
