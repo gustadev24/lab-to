d6621a935c0f4683eb348a7588e3df3adb2e6a44
gustadev24 <lgsc21211@gmail.com>
2025-10-18 12:36:04 -0500

    chore: implement channel for messaging using slots and signals

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -1,20 +1,26 @@
-#include "mainwindow.h"
-
 #include <QApplication>
-#include "adulto.h"
+#include "comunicacion.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    // MainWindow w;
+    // w.show();
 
-    Adulto *papa = new Adulto;
-    Adulto *hijo1 = new Adulto(papa);
-    Adulto *hijo2 = new Adulto(papa);
-    hijo1->setNombre("Manuel");
-    hijo2->setNombre("Jose");
-    delete papa;
+    QObject *person = new QObject;
+    Comunicacion *pepe = new Comunicacion(person);
+    Comunicacion *jose = new Comunicacion(person);
+
+    pepe->setNombre("Pepe");
+    jose->setNombre("Jose");
+
+    QObject::connect(pepe, SIGNAL(enviarmensaje(QString)), jose, SLOT(escuchar(QString)));
+    QObject::connect(jose, SIGNAL(enviarmensaje(QString)), pepe, SLOT(escuchar(QString)));
+
+    pepe->conversar("Buenos días");
+    jose->conversar("¿Cómo vas?");
+
+    delete person;
 
     return a.exec();
 }
