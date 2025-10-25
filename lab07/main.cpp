7ab24fca85cbaaa9c3373c3df98156eb81a5ea8d
gustadev24 <lgsc21211@gmail.com>
2025-10-25 07:57:13 -0500

    task: add main interface with label, button and vertical layout

--- lab07/main.cpp
+++ lab07/main.cpp
@@ -1,12 +1,12 @@
+#include "mainwindow.h"
 #include <QApplication>
 #include <QDebug>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    std::vector<int> v {5,2,3,7,1,0,14,20};
-    std::for_each(v.begin(), v.end(), [](int valor) {
-        qDebug() << valor << Qt::endl;
-    });
+    MainWindow w;
+    w.show();
+
     return a.exec();
 
 }
